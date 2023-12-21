c62394acb809e0ee20dbda199fb00a70c119ad05
marioalija <marioalija25@gmail.com>
2023-12-21 16:22:28 -0600

    updated comments actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,34 @@ class CommentsController < ApplicationController
     @comments = Comment.all
     render :index
   end
+
+  def show
+    @comment = Comment.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @comment = Comment.create(
+      user_id: params[:user_id],
+      story_id: params[:story_id],
+      body: params[:body],
+    )
+    if @comment.valid? #happy path
+      render :show
+    else #sad path
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    @comment.update(
+      body: params[:body] || @story.body,
+    )
+    if @comment.valid? #happy path
+      render :show
+    else #sad path
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end

ad9c4fb5feb53011828b42f5b536c5d2fb4e7241
marioalija <marioalija25@gmail.com>
2023-12-22 13:32:05 -0600

    updated authentications to need a user logged in, beside index and show

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(name: "Admin", email: "admin@example.com", password: "password", image: "image_url")
+    post "/sessions.json", params: { email: "admin@example.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/stories.json"
     assert_response 200
@@ -11,7 +18,8 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Story.count", 1 do
-      post "/stories.json", params: { name: "lake", width: 800, height: 600 }
+      post "/stories.json", headers: { "Authorization" => "Bearer #{@jwt}" }, params: { name: "test", password: "password", image: "image_url", user_id: User.first.id }
+      data = JSON.parse(response.body)
       assert_response 200
     end
   end
@@ -21,16 +29,23 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "user_id", "name", "image", "description", "category"], data.keys
+    assert_equal ["id", "user", "name", "image", "description", "category", "comments"], data.keys
   end
 
   test "update" do
     story = Story.first
-    patch "/stories/#{story.id}.json", params: { name: "Updated name" }
+    patch "/stories/#{story.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }, params: { name: "Updated name", image: "image", descritption: "descritption", category: "category", comments: "comments" }
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
+    assert_equal stories.image, data["image"]
+    assert_equal stories.description, data["description"]
+    assert_equal stories.category, data["category"]
+    assert_equal stories.comments, data["comments"]
+
+    patch "/stories/#{stories.id}.json"
+    assert_response 401
   end
 
   test "destroy" do
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "create" do
     assert_difference "User.count", 1 do
-      post "/users.json", params: { name: "Test", email: "test@test.com", password_digest: "password", image: "image_url" }
+      post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", image: "image_url" }
       assert_response 201
     end
   end
